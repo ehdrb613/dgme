2834b86e83219f1b8aa95ca004fa26d3cdab0d1f
ehdrb613 <ehdrb0613@gmail.com>
2020-12-17 14:20:35 +0900

    dd

---
+++ study/c/201217/201217_1/201217_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _201217_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Java : System.out.prinln("Hello World");
+            Console.WriteLine("hi");
+            Console.WriteLine("Hello World!");
+
+            string ss = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+        }
+    }
+}
