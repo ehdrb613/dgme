e1b77f8545ef1d692ddf65e1cc84d0c3be82f116
ehdrb613 <ehdrb0613@gmail.com>
2020-12-18 13:58:03 +0900

    dd

---
+++ study/c/201218/test01/Algorithm_20201218/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace Algorithm_20201218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("문제풀이시작");
+            Console.WriteLine("1번문제");
+            int sum = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                }
+                
+            }
+            Console.WriteLine($"1부터100까지의 수중 3의배수의 합은:{sum}이다");
+
+            Console.WriteLine("2번 문제");
+            int inputSecond = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{inputSecond/60}분 {inputSecond%60}초");
+
+
+            Console.WriteLine("3번 문제");
+            Console.WriteLine("몇층까지 쌓을 것인가?");
+            int floor = int.Parse(Console.ReadLine());
+            for (int i = 0; i < floor; i++) //전체 층을 출력
+            {
+                //공백을 찍는 부분
+                for (int j = floor; j>i ; j--) //각 층을 출력(공백)
+                {
+                    Console.Write(' ');
+                }
+                for (int k = 0; k < (2*i)+1; k++)
+                {
+                    Console.Write('*');
+                }
+                //별을 찍는 부분
+
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("4번 문제");
+
+            for (int i = 2; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($"{i}x{j}={i*j}");
+                }
+            }
+
+            Console.WriteLine("5번 문제");
+            int month = int.Parse(Console.ReadLine());
+            switch(month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을");
+                    break;
+                default:
+                    Console.WriteLine("잘못된값");
+                    break;
+            }
+
+            Console.WriteLine("6번 문제");
+            int input1 = int.Parse(Console.ReadLine());
+            int input2 = int.Parse(Console.ReadLine());
+
+            if (input1>0 && input2>0)
+                Console.WriteLine("1");
+            else if (input1<0 && input2>0)
+                Console.WriteLine("2");
+            else if (input1<0 && input2<0)
+                Console.WriteLine("3");
+            else if (input1>0 && input2<0)
+                Console.WriteLine("4");
+            else
+                Console.WriteLine("0");
+
+            Console.WriteLine("7번 문제");
+            string start = "1";
+            for (int i = 0; i < 20; i++)
+            {
+                int count = 0; // 각자리 숫자의 개수
+                string end = "";// 문자열을 누적시키는 변수
+                Console.WriteLine($"{i+1}번째 : {start}"); //말하기
+                char number = start[0]; //내가 가르키는 숫자
+
+                for (int j = 0; j < start.Length; j++) //수열 읽어들이기
+                {
+                    if (number != start[j])
+                    {
+                        end = end + number + count;//end = ""+'1'+1
+                        number = start[j];//가르키는 숫자를 다른 걸로 바꿈
+                        count = 1;//count는 1로 초기화, 왜냐면 다른 숫자가 있어서 이 조건문으로 들어왔으니까
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                
+                }
+                end = end + number + count;
+                start = end;
+            }
+
+
+            //8번
+            int input = int.Parse(Console.ReadLine());
+            int max = 1; //각 벌집의 최대값
+            int counter = 0;
+            while (true)
+            {
+                max = max + (6 * counter);
+                if (max < input)
+                {
+                    counter++;
+                    continue;
+                }
+                else
+                    Console.WriteLine($"{input}은 {counter + 1}번째 벌집에 속함");
+                
+                break;
+            }
+
+        }
+    }
+}
--- study/c/201218/test01/test01/Program.cs
+++ study/c/201218/test01/test01/Program.cs
@@ -7,6 +7,106 @@ namespace test01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //cw입력tap키 2번
+            //프로그램 실행 
+            //java : ctrl + F11
+            //ctrl : ctrl + F5
+            Console.Write("Hellow하고 한줄 안띄움");
+            //아무것도 안하고 그냥 한줄 띄우기
+            Console.WriteLine();
+
+            //변수
+            //java와 동일
+
+            int example = 100;
+            //단 String 변수는 string 이랑 String 둘다 동일하게 쓸 수 있음
+            //C#에서는 보통 S 소문자로 적는다.
+            string ex1 = "하이";
+            string ex2 = "헬로";
+            string 이런것도됨 = "신기하죠?";
+            Console.WriteLine(이런것도됨); //한글 쓰지 마요
+            string ex3 = example + "더하기" + example + "은" + (example + example) + "이다";
+            string ex4 = $"{example}더하기 {example}은 {example + example}이다";
+            string ex5 = string.Format("{0}더하기 {1}는 {2}이다", example, example, example + example);
+            Console.WriteLine(ex3);
+            Console.WriteLine(ex4);
+            Console.WriteLine(ex5);
+
+            //조건문
+            //java와 동일함
+
+            if (example>50)
+            {
+                Console.WriteLine("example은 50이상");
+            }
+            if (example>30)
+            {
+                Console.WriteLine("30이상");
+            }
+            else
+            {
+                Console.WriteLine("30미만");
+            }
+
+            if (example==10)
+            {
+                Console.WriteLine("10");
+            }
+            else if(example==20)
+            {
+                Console.WriteLine("20");
+            }//*끝에 꼭 else 안붙여도 됨*
+
+
+            //switch문도 java와 동일
+            switch (example)
+            {
+                case 10:
+                    Console.WriteLine("10");
+                    break;
+                case 20:
+                    Console.WriteLine("20");
+                    break;
+                default:
+                    Console.WriteLine("10이랑 20은 아니다");
+                    break;
+
+            }
+
+            //반복문도 Java와 동일
+            //단 foreach라는 특수 문법이 있는데 이건 나중에 알려줌
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("같은 말 10번 반복하기");
+            }
+
+            int temp = 10;
+            while (temp > 0)
+            {
+                temp--;
+            }
+            do
+            {
+                temp++;
+            } while (temp<10);
+
+            //값을 입력 받는 경우
+            //java
+            /*
+             * Scanner s = new Scanner(System.in);
+             * String test = s.nextLine();
+             * int test2 = s.nextInt();
+             */
+
+            //c#
+            //string test = Console.ReadLine();
+            //int test2 = int.Parse(Console.ReadLine());
+            //Console.ReadLine은 단축키 없음
+
+            string test = Console.ReadLine();
+            int test2 = int.Parse(Console.ReadLine());
+
+
         }
     }
 }
