b804ed651e54ca0a2d418a364668025d28a1b024
ehdrb613 <ehdrb0613@gmail.com>
2020-12-29 16:40:58 +0900

    dd

---
+++ study/c/201228/test1/main/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+                int[] num = new int[9];//입력받을 숫자 갯수
+                int h = 0;//배열 최대값 넣어주는 변수
+                int hNum = 0;//최대값 배열 위치 변수
+
+
+                for (int i = 0; i < 9; i++)
+                //숫자 num개 만큼 입력받음
+                {
+                    num[i] = int.Parse(Console.ReadLine());
+                }
+
+                h = num[0];
+
+
+                for (int i = 0; i < 9; i++)
+                {
+
+                    if (h < num[i])
+                    //h(최대값)
+                    //처음 h 초기값이 num[0] 이고 다음 
+                    //num[i]값과 비교해서 h가 작으면 num[i]가
+                    //큰것이므로 h변수에 num[i]넣고 다음 반복엔
+                    //바뀐 값으로 다음 i+1 값과 다시 비교
+                    {
+                        h = num[i];
+                        hNum = i;
+                    
+                    }
+
+
+                }
+
+                Console.WriteLine(h);
+                Console.WriteLine(hNum + 1);
+
+
+        }
+    }
+}
+
+    
+
---
+++ study/c/201228/test1/main2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string a = Console.ReadLine();
+           
+            string b = Console.ReadLine();
+        
+            string c = Console.ReadLine();
+            //여기까지 값 a b c 입력 받는 부분
+
+            string abc = (int.Parse(a) * int.Parse(b) * int.Parse(c)).ToString();
+            //string으로 입력 받아서 int.Parse 해줘야 곱하기 가능하고  곱하고하고 난뒤 다시 ToString() 이용해서 string 형으로 바꿈
+            int[] count = new int[10];
+            //0~9개의 수의 갯수를 파악하기 위해 배열로 만들어놓음.. ex)count[0] 에는 숫자 0의 개수가 몇개인지
+            //배열 안할려면 변수 9개 만들어야됨
+
+            for (int i = 0; i < 10; i++) // 숫자 0에서 부터 9까지 갯수를 파악하려면 10번 loop 해야됨
+            {
+                for (int j = 0; j < abc.Length; j++)
+                //abc.Length는 abc의 길이값을 넣어주는 명령어
+                //ex) abc = 10101010 이면 abc.Length 는 8
+
+                {
+
+                    if (i == abc[j] - '0')
+                    // i변수는 int형이고 abc[j]는 char문자 형이기 때문에 - '0'(아스키코드 0은 숫자48) 빼주면 int형과 같아짐
+                    // i변수가 0일때 abc[0] 배열이 '0'(숫자 48로 인식)이면 "==" 이거 써도 조건에 맞지 않기 때문에 -'0' 꼭해줘야됨
+                    // i가 0이고 abc[0]=0 이면 카운트 하기 위한 조건문
+                    // 10101010 이면 0번째가 1이기 때문에 0은 카운트 안되고 1번째에 0이기 때문에 카운트
+                    {
+                        count[i] += 1;  //카운트
+                    }
+
+                }
+
+            }
+          
+            for (int i = 0; i < count.Length; i++)
+            //깔끔하게 보여주기 위해서 한번더 하는 for문
+            // 대충하려면 첫번째 for문 안에 출력하면 가능
+            //마찬가지로 count 길이 만큼 반복
+            {
+                Console.WriteLine(count[i]);
+                //count[i]배열 안에 값이 0보다 많으면 출력
+                //그냥 0개인데 나오면 지저분하기 때문에 연습문제 출력처럼 나올려면 해야됨
+            }
+        }
+    }
+}
